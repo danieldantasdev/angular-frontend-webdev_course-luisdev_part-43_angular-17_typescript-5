b722f511e82c04def8bf962504a862031648d9d0
Daniel Dantas <contatodanieldantasdev@gmail.com>
2026-01-04 23:38:22 -0300

    feat: adição do arquivo aula_1.ts

---
+++ developments/ld-app-2/aula_1.ts
@@ -0,0 +1,21 @@
+// let test = 1;
+
+// test = 'Valor string';
+
+// function sum(a: number, b: number){
+//     return a + b;
+// }
+
+// sum('a', 'b')
+
+interface IPerson {
+    name: string;
+    age: number;
+}
+
+let joao: IPerson;
+
+joao = {
+    name: 'João',
+    age: 5
+}
