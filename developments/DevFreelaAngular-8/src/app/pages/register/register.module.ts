99383cd49dbcdc83c09c319400c91d29880606fe
Daniel Dantas <contatodanieldantasdev@gmail.com>
2026-01-04 23:54:43 -0300

    feat: adição do arquivo register.module.ts

---
+++ developments/DevFreelaAngular-8/src/app/pages/register/register.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register.component';
+import { RegisterRoutingModule } from './register-routing.module';
+import { LdHeaderModule } from 'src/app/features/ld-header/ld-header.module';
+import { LdButtonModule } from 'src/app/shared/components/ld-button/ld-button.module';
+
+@NgModule({
+  declarations: [
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    RegisterRoutingModule,
+    LdHeaderModule,
+    LdButtonModule
+  ]
+})
+
+export class RegisterModule { }
