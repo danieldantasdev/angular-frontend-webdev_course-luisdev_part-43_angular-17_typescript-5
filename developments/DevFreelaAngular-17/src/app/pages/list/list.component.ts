6cf74878457327ddf049a296fa5505c03a11df6d
Daniel Dantas <contatodanieldantasdev@gmail.com>
2026-01-04 23:44:51 -0300

    feat: adição do arquivo list.component.ts

---
+++ developments/DevFreelaAngular-17/src/app/pages/list/list.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { IListItem } from './interfaces/IListItem';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+  constructor() { }
+
+  list: IListItem[] = [];
+
+  ngOnInit(): void {
+    this.getProjects();
+  }
+
+  getProjects() {
+    fetch("https://622cd1e6087e0e041e147214.mockapi.io/api/projects")
+      .then(response => response.json())
+      .then((response: IListItem[]) => {
+        this.list = response;
+        this.buildTable();
+      })
+  }
+
+  buildTable() {
+    (document.querySelector("#table-body") as any).innerHTML = '';
+    const idClient = localStorage.getItem('idClient');
+
+    this.list = this.list.filter((listItem: IListItem) => listItem.idClient === idClient);
+
+    this.list.forEach((listItem: IListItem) => {
+        let template = `
+            <div class="row">
+                <div class="title-description">
+                    <h6 class="title">${listItem.title}</h6>
+                    <p class="description">${listItem.description}</p>
+                </div>
+                <div class="price">R$ ${listItem.totalCost}</div>
+                <div class="actions">
+                    <span class="edit material-icons" onclick="goToEdit(${listItem.id})">edit</span>
+                    <span class="delete material-icons" onclick="deleteProject(${listItem.id})">delete_outline</span>
+                </div>
+            </div>
+        `;
+
+        (document.querySelector("#table-body") as any).insertAdjacentHTML("beforeend", template)
+    });
+}
+
+ goToEdit(id: string) {
+   window.location.href = `project-create-edit.html?id=${id}`;
+ }
+
+ deleteProject(id: string) {
+   fetch(`https://622cd1e6087e0e041e147214.mockapi.io/api/projects/${id}`, {
+       method: 'DELETE'
+     })
+     .then(response => response.json())
+     .then(response => {
+       this.list = this.list.filter((project: IListItem) => project.id != id);
+
+       this.buildTable();
+     })
+ }
+
+ }
